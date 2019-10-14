14b4ee1aeab6d6998e64957bc46d7faecb7fa246
Zachary Dremann <dremann@gmail.com>
2019-10-13 21:00:15 -0400

    Some more docs

--- repr/src/uid_gid.rs
+++ repr/src/uid_gid.rs
@@ -1,8 +1,11 @@
+//! User/Group IDs
+
 use packed_serialize::PackedStruct;
 
 /// UID/GIDs are both stored as u32s. Both UIDs and GIDs are treated as IDs
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PackedStruct)]
 pub struct Id(u32);
 
+/// The index of a user ID in the uid_gid list
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PackedStruct)]
 pub struct Idx(u16);
