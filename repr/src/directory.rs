508fcbca0a20d14611e45df98688067a92d27ee8
Zachary Dremann <dremann@gmail.com>
2019-10-27 17:41:13 -0400

    More directory docs

--- repr/src/directory.rs
+++ repr/src/directory.rs
@@ -1,10 +1,26 @@
 //! Directory Table
 //!
 //! Directory listings, including file names, and references to inodes
+//!
+//! For each directory inode, the directory table stores a list of all entries stored inside, with
+//! references back to the inodes that describe those entries.
+//
+//The entry list is self is sorted ASCIIbetically by entry name. To save space, a delta encoding is
+// used to store the inode number, i.e. the list is preceded by a header with a reference inode
+// number and all entries store the difference to that. Furthermore, the header also includes the
+// location of a metadata block that the inodes of all of the following entries are in.
+// The entries just store an offset into the uncompressed metadata block.
 
 use crate::inode;
 use packed_serialize::PackedStruct;
 
+/// A header which precedes a list of directory entries
+///
+///Every time, the inode block changes or the difference of the inode number cannot be encoded in
+/// 16 bits anymore, a new header is emitted.
+///
+///A header must not be followed by more than 256 entries. If there are more entries,
+/// a new header is emitted.
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PackedStruct)]
 pub struct Header {
     /// Number of entries following the header
@@ -30,6 +46,9 @@ pub struct Header {
 /// all the directory entries (including all headers) belonging to the inode.
 /// This field is used to deduce if more data is following while iterating over directory entries,
 /// even without knowing how many headers and partial lists there will be.
+///
+///The file names are stored without trailing null bytes. Since a zero length name makes no sense,
+/// the name length is stored off-by-one, i.e. the value 0 cannot be encoded
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PackedStruct)]
 pub struct Entry {
     /// An offset into the uncompressed inode metadata block
