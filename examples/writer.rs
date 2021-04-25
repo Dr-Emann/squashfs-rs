5993bf49b40cf54d3367b7222045027837f1d7a1
Zachary Dremann <dremann@gmail.com>
2021-04-25 00:09:27 -0400

    Modify example again

--- examples/writer.rs
+++ examples/writer.rs
@@ -8,10 +8,9 @@ fn main() {
         positioned_io::RandomAccessFile::try_new(f).unwrap(),
     ));
     println!("{:#?}", archive);
-    let root = archive.begin_root();
+    let mut root = archive.create_dir();
 
-    let child_dir = root.begin_dir("subdir");
-    let mut child_dir = child_dir.done_subdirs();
+    let mut child_dir = archive.create_dir();
     child_dir.set_mode(sqfs::Mode::O755);
     child_dir.set_uid(1000).set_gid(1000);
     child_dir.set_modified_time(DateTime::from(std::time::UNIX_EPOCH));
@@ -24,13 +23,13 @@ fn main() {
     let file_ref = file.finish(&mut archive);
 
     child_dir.add_item("my_file", file_ref);
-    child_dir.finish(&mut archive);
+    let child_dir_ref = child_dir.finish(&mut archive);
 
-    let mut root = root.done_subdirs();
     root.add_item("my_file_link", file_ref);
+    root.add_item("subdir", child_dir_ref);
 
-    let item = root.finish(&mut archive);
-    archive.set_root(item);
+    let root_ref = root.finish(&mut archive);
+    archive.set_root(root_ref);
     println!("{:#?}", archive);
     archive.flush().expect("Unable to flush");
 }
