c07040db7c8fa24eb829c8cba875a6b751e7e39e
Zachary Dremann <dremann@gmail.com>
2021-08-01 21:16:43 -0400

    Use IndexMap to back uid_gid table

--- src/write/uid_gid.rs
+++ src/write/uid_gid.rs
@@ -1,35 +1,24 @@
 use crate::compress_threads::ParallelCompressor;
-use crate::write::metablock_writer::MetablockWriter;
 use crate::write::two_level;
-use byteorder::{LittleEndian, WriteBytesExt};
-use std::collections::HashMap;
+use indexmap::IndexSet;
 use std::io;
 use std::sync::Arc;
 
 pub struct Table {
-    ids: Vec<repr::uid_gid::Id>,
-    known_ids: HashMap<repr::uid_gid::Id, repr::uid_gid::Idx>,
+    ids: IndexSet<repr::uid_gid::Id>,
 }
 
 impl Table {
     pub fn new() -> Self {
         Self {
-            ids: Vec::new(),
-            known_ids: HashMap::new(),
+            ids: IndexSet::new(),
         }
     }
 
     pub fn add(&mut self, id: repr::uid_gid::Id) -> repr::uid_gid::Idx {
-        if let Some(&idx) = self.known_ids.get(&id) {
-            return idx;
-        }
+        let (idx, _) = self.ids.insert_full(id);
 
-        assert!(self.ids.len() < usize::from(u16::MAX));
-
-        let idx = repr::uid_gid::Idx(self.ids.len() as u16);
-        self.known_ids.insert(id, idx);
-        self.ids.push(id);
-        idx
+        repr::uid_gid::Idx(idx as u16)
     }
 
     pub async fn write_at<W: io::Write>(
